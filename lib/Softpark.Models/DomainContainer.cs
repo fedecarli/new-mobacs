80ebb8e5f90c532a3e3d06533dc78bed0cd10cde
Elton Schivei Costa <elton.costa@softpark.com.br>
2017-05-24 15:05:44 -0300

    Implementação de Assíncronismo e Paralelismo nas chamadas da API ao Banco de Dados

--- lib/Softpark.Models/DomainContainer.cs
+++ lib/Softpark.Models/DomainContainer.cs
@@ -53,41 +53,5 @@ namespace Softpark.Models
         // ReSharper disable once InconsistentNaming
         public virtual DbRawSqlQuery<VW_Profissional> VW_Profissional =>
             Database.SqlQuery<VW_Profissional>("SELECT CBO, CNES, CNS, INE, Equipe, Nome, Profissao, Unidade FROM [api].[VW_Profissional]");
-
-        /// <summary>
-        /// 
-        /// </summary>
-        // ReSharper disable once InconsistentNaming
-        public virtual IQueryable<VW_IdentificacaoUsuarioCidadao_bkp> VW_IdentificacaoUsuarioCidadao_bkp
-            => Database
-                .SqlQuery<VW_IdentificacaoUsuarioCidadao_bkp>("SELECT id, nomeSocial, codigoIbgeMunicipioNascimento, dataNascimentoCidadao, desconheceNomeMae, emailCidadao, nacionalidadeCidadao, nomeCidadao, nomeMaeCidadao, cnsCidadao, cnsResponsavelFamiliar, telefoneCelular, numeroNisPisPasep, paisNascimento, racaCorCidadao, sexoCidadao, statusEhResponsavel, etnia, num_contrato, nomePaiCidadao, desconheceNomePai, dtNaturalizacao, portariaNaturalizacao, dtEntradaBrasil, microarea, stForaArea, CBO, CNES, INE, Codigo FROM [api].[VW_IdentificacaoUsuarioCidadao] WHERE id IS NOT NULL")
-                .AsQueryable();
-
-        /// <summary>
-        /// 
-        /// </summary>
-        // ReSharper disable once InconsistentNaming
-        public virtual DbRawSqlQuery<VW_ultimo_cadastroDomiciliar_bkp> VW_ultimo_cadastroDomiciliar_bkp
-            => Database
-                .SqlQuery<VW_ultimo_cadastroDomiciliar_bkp>("SELECT idCadastroDomiciliar, headerTransport, token, idAuto FROM [api].[VW_ultimo_cadastroDomiciliar]");
-
-        /// <summary>
-        /// 
-        /// </summary>
-        // ReSharper disable once InconsistentNaming
-        public virtual IQueryable<VW_profissional_cns_bkp> VW_profissional_cns_bkp
-            => Database
-                .SqlQuery<VW_profissional_cns_bkp>("SELECT Codigo, idProfissional, cnsProfissional, cnsCidadao, IdCidadao, CNES, CBO, INE, CodigoCidadao FROM [api].[VW_profissional_cns]")
-                .AsQueryable();
-
-        /// <summary>
-        /// 
-        /// </summary>
-        // ReSharper disable once InconsistentNaming
-        public virtual IQueryable<VW_ultimo_cadastroIndividual_bkp> VW_ultimo_cadastroIndividual_bkp
-            => Database
-                .SqlQuery<VW_ultimo_cadastroIndividual_bkp>(
-                    "SELECT Codigo, idCadastroIndividual, headerTransport, token FROM [api].[VW_ultimo_cadastroIndividual]")
-                .AsQueryable();
     }
 }
