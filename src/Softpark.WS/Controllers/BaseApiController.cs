45c7289fb1712e872c2e87b0333661cc27d666e1
Elton Schivei Costa <elton.costa@softpark.com.br>
2017-05-26 10:26:44 -0300

    Rebase to 1.4.2.1

--- src/Softpark.WS/Controllers/BaseApiController.cs
+++ src/Softpark.WS/Controllers/BaseApiController.cs
@@ -12,10 +12,5 @@ namespace Softpark.WS.Controllers
         private static readonly ILog log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
 
         protected static ILog Log => log;
-        
-        /// <summary>
-        /// Domain models
-        /// </summary>
-        protected Repository Repository => new Repository();
     }
 }
