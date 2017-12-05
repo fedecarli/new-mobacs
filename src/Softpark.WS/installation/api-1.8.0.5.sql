7b767b091bd0536ae0c9339fdd0d0897370a09bf
Elton Schivei Costa <elton.costa@softpark.com.br>
2017-12-05 19:19:25 -0200

    version-1.8.0.5

---
+++ src/Softpark.WS/installation/api-1.8.0.5.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [api].[Documentos] (
+	Id uniqueidentifier NOT NULL DEFAULT (NEWID()) PRIMARY KEY,
+	IdIdentificacaoUsuarioCidadao uniqueidentifier NOT NULL REFERENCES api.IdentificacaoUsuarioCidadao (Id),
+	NumContrato int NOT NULL DEFAULT (22),
+	IdTipoDocumento int NOT NULL,
+	TipoArquivo varchar(60) NOT NULL,
+	Tamanho bigint NOT NULL,
+	[Data] datetime NOT NULL DEFAULT (GETDATE()),
+	Arquivo varbinary(MAX)
+) ON [PRIMARY]
+GO
+
+ALTER TABLE api.Documentos
+	 ADD FOREIGN KEY (NumContrato, IdTipoDocumento) REFERENCES ASSMED_TipoDocPessoal (NumContrato, CodTpDocP)
+GO
